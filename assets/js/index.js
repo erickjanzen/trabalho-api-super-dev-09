64d83b8621682ee1a47d5c41629b060604ad03ac
Erick Janzen <erickjanzen554@gmail.com>
2026-05-31 00:27:37 -0300

    adicionado Create POST, concluir botao de calcular total analisando o Back End e fazer os outro botoes

---
+++ assets/js/index.js
@@ -0,0 +1,81 @@
+const botaoEnviar = document.getElementsByClassName('enviarReserva')[0];
+botaoEnviar.addEventListener('click', enviarReservas)
+
+function enviarReservas(evento){
+    const areaNome = document.getElementById("areaNome")
+    const tipoServico = document.getElementById("tipoServico")
+    const detalhesCorte = document.getElementById("detalhesCorte").value
+    const urlBase = "https://api.franciscosensaulas.com/api/v1/trabalho/atendimentos"
+    const atendente = document.getElementById("atendente").value;
+    const horarioAgendado = document.getElementById("horarioAgendado").value;
+    const horario = parseFloat(horarioAgendado);
+
+    const nome = areaNome.value.trim();
+    let valorPedido = 0;
+
+    if(nome.length < 2){
+        alert("O nome deve conter pelo menos 2 caracteres.");
+        return;
+    }
+    debugger
+    if(tipoServico.value === "cabelo"){
+        valorPedido = 50
+    } else if (tipoServico.value === "barba"){
+        valorPedido = 45
+    } else if (tipoServico.value === "cabeloEBarba"){
+        valorPedido = 90
+    } else if (tipoServico.value === "sobrancelha"){
+        valorPedido = 20
+    } else if (tipoServico.value === "completo"){
+        valorPedido = 100
+    } else if (tipoServico.value === "cabeloESobrancelha"){
+        valorPedido = 60
+    } else if (tipoServico.value === "barbaESobrancelha"){
+        valorPedido = 55
+    } else {
+        alert("Selecione um tipo de serviço.");
+        return;
+    }
+
+    if(atendente === ""){
+        alert("Selecione um atendente.");
+        return;
+    }
+
+    if(isNaN(horario) || horario <= 0){
+        alert("Selecione um horário para o atendimento.");
+        return;
+    }
+
+    const dados = {
+        cliente: nome,
+        tipoAtendimento: tipoServico.value,
+        descricao: detalhesCorte,
+        atendente: atendente,
+        duracaoMinutos: horario
+    }
+
+    console.log(dados);
+    console.log(JSON.stringify(dados));
+
+    fetch(urlBase, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+    })
+
+    const infoServico = document.getElementById("infoServico");
+    infoServico.innerText = `Nome do cliente: ${areaNome.value}
+    Valor do pedido: R$ ${valorPedido.toFixed(2)}
+    Detalhes do serviço: ${detalhesCorte}
+    Atendente: ${atendente}
+    Horário agendado: ${horarioAgendado}h`;
+
+    infoServico.classList.remove("hidden");
+
+    const botaoFinal = document.getElementById("botaoFinal");
+    botaoFinal.innerText = "Enviar Pedido";
+
+}
