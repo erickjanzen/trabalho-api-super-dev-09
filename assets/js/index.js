7e926072f0d7a1d6ff50bbed593c63a6b28c3320
Erick Janzen <erickjanzen554@gmail.com>
2026-06-01 09:08:53 -0300

    adicionado botao de apagar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
 const botaoEnviar = document.getElementsByClassName('enviarReserva')[0];
 botaoEnviar.addEventListener('click', enviarReservas)
 const urlBase = "https://api.franciscosensaulas.com/api/v1/trabalho/atendimentos";
-function enviarReservas(evento){
+const tipoServico = document.getElementById("tipoServico")
+
+function enviarReservas(){
     const areaNome = document.getElementById("areaNome")
-    const tipoServico = document.getElementById("tipoServico")
     const detalhesCorte = document.getElementById("detalhesCorte").value
     const atendente = document.getElementById("atendente").value;
     const horarioAgendado = document.getElementById("horarioAgendado").value;
@@ -17,25 +18,6 @@ function enviarReservas(evento){
         return;
     }
     
-    if(tipoServico.value === "cabelo"){
-        valorPedido = 50
-    } else if (tipoServico.value === "barba"){
-        valorPedido = 45
-    } else if (tipoServico.value === "cabeloEBarba"){
-        valorPedido = 90
-    } else if (tipoServico.value === "sobrancelha"){
-        valorPedido = 20
-    } else if (tipoServico.value === "completo"){
-        valorPedido = 100
-    } else if (tipoServico.value === "cabeloESobrancelha"){
-        valorPedido = 60
-    } else if (tipoServico.value === "barbaESobrancelha"){
-        valorPedido = 55
-    } else {
-        alert("Selecione um tipo de serviço.");
-        return;
-    }
-
     if(atendente === ""){
         alert("Selecione um atendente.");
         return;
@@ -68,28 +50,84 @@ function enviarReservas(evento){
 .then(res => {
 
     console.log("Resposta da API:", res);
-
-    // SALVA O ID PARA A OUTRA PÁGINA
     localStorage.setItem("atendimentoId", res.id);
+    alert("Pedido cadastrado com sucesso");
+    location.reload();
 
-})
-.then(res => {
-    console.log("Resposta da API:", res);
 })
 .catch(error => {
     console.log("Erro ao enviar:", error);
+      alert("Ocorreu um erro ao cadastrar o pedido")
 });
+}
+
+const botaoApresentar = document.getElementById("botaoApresentar")
+botaoApresentar.addEventListener('click', apresentarInfos)
+
+function apresentarInfos(){
+
+    if(tipoServico.value === "cabelo"){
+        valorPedido = 50
+    } else if (tipoServico.value === "barba"){
+        valorPedido = 45
+    } else if (tipoServico.value === "cabeloEBarba"){
+        valorPedido = 90
+    } else if (tipoServico.value === "sobrancelha"){
+        valorPedido = 20
+    } else if (tipoServico.value === "completo"){
+        valorPedido = 100
+    } else if (tipoServico.value === "cabeloESobrancelha"){
+        valorPedido = 60
+    } else if (tipoServico.value === "barbaESobrancelha"){
+        valorPedido = 55
+    } else {
+        alert("Selecione um tipo de serviço.");
+        return;
+    }
+
 
     const infoServico = document.getElementById("infoServico");
     infoServico.innerText = `Nome do cliente: ${areaNome.value}
     Valor do pedido: R$ ${valorPedido.toFixed(2)}
-    Detalhes do serviço: ${detalhesCorte}
-    Atendente: ${atendente}
-    Horário agendado: ${horarioAgendado}h`;
+    Detalhes do serviço: ${detalhesCorte.value}
+    Atendente: ${atendente.value}
+    Horário agendado: ${horarioAgendado.value}h`;
 
     infoServico.classList.remove("hidden");
+}
 
-    const botaoFinal = document.getElementById("botaoFinal");
-    botaoFinal.innerText = "Enviar Pedido";
+const botaoApagar = document.getElementById("botaoApagar");
+botaoApagar.addEventListener('click', apagarCadastro);
 
+function apagarCadastro(){
+
+    const idApagar = localStorage.getItem("atendimentoId");
+
+    if (!idApagar) {
+        alert("Nenhum registro encontrado.");
+        return;
+    }
+
+    const confirmacaoApagar = confirm("Deseja realmente apagar?");
+
+    if(confirmacaoApagar !== true){
+        return;
+    }
+
+    const url = `${urlBase}/${idApagar}`
+
+    fetch(url, {
+        method: "DELETE"
+    })
+        .then(response => {
+            if(response.status === 204) {
+                alert("Reserva apagada com sucesso")
+            } else {
+                alert("Não foi possível apagar a reserva")
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao apagar reserva: " + error)
+            alert("Ocorreu um erro ao tentar apagar a reserva");
+        })
 }
